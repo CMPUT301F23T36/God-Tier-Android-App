6a7376c616dc46ae5a144b7cf444634561c32cdb
Alex Harrison <alexander.harrison.r@gmail.com>
2023-11-06 14:48:04 -0700

    Add item description activity

--- app/src/main/java/com/example/godtierandroidapp/Tag.java
+++ app/src/main/java/com/example/godtierandroidapp/Tag.java
@@ -1,6 +1,8 @@
 package com.example.godtierandroidapp;
 
-public class Tag {
+import java.io.Serializable;
+
+public class Tag implements Serializable {
     private String name;
 
     public String getName() {
