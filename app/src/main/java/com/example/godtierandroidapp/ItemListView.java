14e4ec18cf947c97e845eb2c858593d4a0bc55f2
Alex Harrison <alexander.harrison.r@gmail.com>
2023-11-09 14:12:37 -0700

    Add item sorting, start item filtering

--- app/src/main/java/com/example/godtierandroidapp/ItemListView.java
+++ app/src/main/java/com/example/godtierandroidapp/ItemListView.java
@@ -1,11 +1,16 @@
 package com.example.godtierandroidapp;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class ItemListView extends AppCompatActivity {
@@ -19,11 +24,8 @@ public class ItemListView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_list_view);
 
-        // Initialize the RecyclerView
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // init ItemList ---------------------------------------------------------------------------
 
-        // Create a list of Item objects (you can replace this with your actual data)
         itemList = new ItemList();
         ArrayList<Tag> tags = new ArrayList<>(Arrays.asList(
                 new Tag("tag1"),
@@ -32,11 +34,35 @@ public class ItemListView extends AppCompatActivity {
         ));
         itemList.addItem(new Item("Test item 1", 100.0, tags));
         itemList.addItem(new Item("Test item 2", 200.0, new ArrayList<>()));
+        itemList.addItem(new Item("Test item 3", 50.0, new ArrayList<>()));
 
-        // Initialize the custom adapter
+        // init view and ItemListViewAdapter -------------------------------------------------------
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         itemAdapter = new ItemListViewAdapter(this, itemList);
-
-        // Set the adapter for the RecyclerView
         recyclerView.setAdapter(itemAdapter);
+
+        // init button callbacks -------------------------------------------------------------------
+
+        findViewById(R.id.sort_button).setOnClickListener(v -> {
+            SortFragment sortFragment = new SortFragment(this);
+            sortFragment.show(getSupportFragmentManager(), "SortFragment");
+        });
+
+        findViewById(R.id.filter_button).setOnClickListener(v -> {
+            FilterFragment filterFragment = new FilterFragment(this);
+            filterFragment.show(getSupportFragmentManager(), "FilterFragment");
+        });
+    }
+
+    public void setFilter(ItemList.FilterCriteria filterFunction) {
+        itemList.setFilter(filterFunction);
+        itemAdapter.notifyDataSetChanged();
+    }
+
+    public void setSort(Comparator<Item> sortComparator) {
+        itemList.setSort(sortComparator);
+        itemAdapter.notifyDataSetChanged();
     }
 }
